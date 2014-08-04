73f0fd51905de31083e1f1dd108bf524f13288dd
ilynne <ilynne@gmail.com>
2014-08-03 21:13:44 -0700

    initial layout; helpers for auth links and devise links; partials for future use #13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def devise_links
+    if current_user # user_signed_in?
+      link_to 'Log Out User', destroy_user_session_path, method: :delete
+    else
+      link_to 'Log In', new_user_session_path
+    end
+  end
 end
