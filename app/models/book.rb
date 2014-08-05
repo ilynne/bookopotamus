f4da68e5b94fae40b42db97f14e2c4808113d033
ilynne <ilynne@gmail.com>
2014-08-04 21:48:20 -0700

    adds validations to Book #20

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,12 @@ class Book < ActiveRecord::Base
   has_many :ratings
   belongs_to :user
 
+  validates :title, presence: true
+  validates :isbn_10, presence: true, length: { is: 10 }
+  validates :isbn_13, presence: true, length: { is: 13 }
+  validates :author_last, presence: true
+  validates :author_first, presence: true
+
   def average_rating
     ratings.sum(:score).to_f / ratings.size.to_f
   end
