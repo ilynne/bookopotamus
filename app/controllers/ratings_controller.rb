23728a757c3695a8efba8d3b3ccb1ca74e52276a
ilynne <ilynne@gmail.com>
2014-08-13 20:26:40 -0700

    move ratings url from js to partial #29

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,20 @@
 class RatingsController < ApplicationController
+  before_filter only: [:new] do
+    redirect_to :new_user_session unless current_user
+  end
+
   def index
+  end
+
+  # GET /books/id/ratings/new
+  def new
+    @book = Book.find(params[:id])
+    @rating = @book.ratings.find_or_create(user: current_user, score: params[:score], book: @book)
+    @rating.save
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def rating_params
+    params[:rating].permit(:score, :book_id, :user_id,)
   end
 end
