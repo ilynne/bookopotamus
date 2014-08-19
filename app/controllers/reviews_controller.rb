451510c52321f9b422b76e0cf9b643fff45d5c56
ilynne <ilynne@gmail.com>
2014-08-18 21:32:38 -0700

    adds update method to reviews controller #30

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,22 @@
 class ReviewsController < ApplicationController
-
   def index
     @reviews = Review.all
   end
 
+  def update
+    @review = Review.find(params[:id])
+    @book = @review.book
+    if @review.update(review_params)
+      redirect_to @book, notice: 'Review was successfully updated.'
+    else
+      render redirect_to @book, notice: 'Error'
+    end
+  end
+
 private
 
-  # def review_params
-  #   params[:review].permit(:body, :user_id, :book_id)
-  # end
+  def review_params
+    params[:review].permit(:body, :user_id, :book_id)
+  end
 
 end
