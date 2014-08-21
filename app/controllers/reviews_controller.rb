8ecb9cb91868f04fd857a9b8d25b0be48bbfc509
ilynne <ilynne@gmail.com>
2014-08-20 18:48:22 -0700

    return before save or update if book inactive #33

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,8 @@ class ReviewsController < ApplicationController
     @review = Review.find_or_create_by(book_id: params[:review][:book_id], user_id: params[:review][:user_id])
     @review.body = params[:review][:body]
     @book = Book.find(params[:review][:book_id])
-    if @book.active && @review.save
+    return unless @book.active
+    if @review.save
       redirect_to @book, notice: 'Review was successfully submitted.'
     else
       redirect_to @book, notice: 'Error'
@@ -17,17 +18,17 @@ class ReviewsController < ApplicationController
   def update
     @review = Review.find(params[:id])
     @book = @review.book
-    if @book.active && @review.update(review_params)
+    return unless @book.active
+    if @review.update(review_params)
       redirect_to @book, notice: 'Review was successfully updated.'
     else
       redirect_to @book, notice: 'Error'
     end
   end
 
-private
+  private
 
   def review_params
     params[:review].permit(:body, :user_id, :book_id)
   end
-
 end
