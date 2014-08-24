fdb02a15c6ab3dced743229d3fe6e872b345d2c9
ilynne <ilynne@gmail.com>
2014-08-24 02:01:36 -0700

    controller changes for quality #42

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
+        format.html { redirect_to @user, notice: 'User privileges changed.' }
+        format.json { render :show, status: :ok }
       else
         format.html { render :edit }
         format.json {}
