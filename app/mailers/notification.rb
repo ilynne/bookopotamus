bb2cf1080b5464ddf127b16c22faea9a1301c295
ilynne <ilynne@gmail.com>
2014-08-24 14:51:29 -0700

    Adds basic welcome email #44
    adds Figaro application.yml
    adds mail config for development

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -1,3 +1,8 @@
 class Notification < ActionMailer::Base
-  default from: "from@example.com"
+  default from: 'lynne@ilynne.com'
+
+  def welcome_email(user)
+    @user = user
+    mail(:to => @user.email, subject: 'Welcome to Bookopotamus!', body: 'Welcome!')
+  end
 end
