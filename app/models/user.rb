4443bd9f7cf822491079db045b27ae654bde5901
ilynne <ilynne@gmail.com>
2015-05-26 10:28:14 -0700

    adds api_key generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
   has_many :books
   has_many :follows
   has_one :email_prefs
+
+  def generate_api_key
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qat')
+      break token unless User.exists?(api_key: token)
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,9 @@ describe User do
     it 'a new user should not have an api_key' do
       expect(user.api_key).to eq(nil)
     end
-    # it 'should generate an api_key' do
-    # end
+    it 'should generate an api_key' do
+      # puts user.generate_api_key
+      expect(user.generate_api_key).not_to eq(nil)
+    end
   end
 end
