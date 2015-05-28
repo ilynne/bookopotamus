4080af7fd9e07d731e78f9f71aa2ac00179665f3
ilynne <ilynne@gmail.com>
2015-05-28 09:13:24 -0700

    applies bootstrap 3 to most views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,4 +37,17 @@ module ApplicationHelper
         flash_type.to_s
     end
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
